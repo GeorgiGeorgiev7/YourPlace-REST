39480cb2806e2ff8dc69146e222c1375cde564d6
Georgi Georgiev <georgingeorgiev7@gmail.com>
2022-02-11 16:21:42 +0200

    Removed places from User model

--- models/User.js
+++ models/User.js
@@ -6,8 +6,7 @@ const userSchema = new Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, minlength: 6 },
-    image: { type: String, required: true },
-    places: [{ type: ObjectId, required: true, ref: 'Place' }]
+    image: { type: String, required: true }
 });
 
 userSchema.plugin(uniqueValidator);
